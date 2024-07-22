3871cf2e5c122e2189b6014f7acb4bae44c754b7
Euan Prentis <euanprentis@Euans-iMac.local>
2024-07-22 16:50:09 -0500

    larger trial experiment

--- scripts/simulate_config.py
+++ scripts/simulate_config.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 # Output directory
-OUTPUT_PATH = '/Users/euanprentis/Documents/feat_predict_simulations/data'
+OUTPUT_PATH = '/Users/euanprentis/Documents/feat_predict_simulations/data_2'
 
 # Random seed for reproducibility
 SEED = 3244343
 
 # Number of training trials
-N_TRAINING_TARGET_REPEATS = 6
+N_TRAINING_TARGET_REPEATS = 40
 
 # Simulate based on existing agent configurations
 LOAD_AGENT_CONFIGS = False
